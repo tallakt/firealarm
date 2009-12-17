ef4ab617c5c62ccf7b90f0a9afc8c2ba4c08090b
Tallak Tveide <tallak@tveide.net>
2009-12-17 09:27:07 +0100

    First commit

---
+++ firealarm.rb
@@ -0,0 +1,30 @@
+#!/usr/bin/env ruby
+
+require 'rubygems'
+require 'rmodbus'
+require 'clickatell'
+
+mb = ModBus::TCPClient.new '192.168.0.189'
+
+clickatell = Clickatell::API.authenticate 'xxxxxx', 'xxxxxx', 'xxxxxx'
+
+begin
+  loop do
+    ra, rb = mb.read_holding_registers 5391, 2
+    a, b, c, d = (0..3).map {|bit| ra[bit] }
+
+    puts 'a: %s' % a.to_s
+    puts 'b: %s' % b.to_s
+    puts 'c: %s' % c.to_s
+    puts 'd: %s' % d.to_s
+
+    clickatell.send_message '4740220423', 'BRANN!' if a == 0
+
+#    [5356, 5366, 5357, 5382, 5383, 5391, 5392].each do |addr|
+#      puts '%d: %d' % [addr, mb.read_holding_registers(addr, 1).first]
+#    end
+    sleep 10.0
+  end
+ensure
+  mb.close
+end
