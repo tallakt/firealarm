6ebda69c0e844137024575a8f828a645225bb74a
Tallak Tveide <tallak@tveide.net>
2009-12-29 22:09:31 +0100

    A bit more robust

--- firealarm.rb
+++ firealarm.rb
@@ -10,10 +10,12 @@ mb = ModBus::TCPClient.new '192.168.0.189'
 
 cc = YAML.load_file File.join(Etc.getpwuid.dir, '.clickatell')
 clickatell = Clickatell::API.authenticate cc['api_key'], cc['username'], cc['password']
+fire_warned = false
 
 begin
   loop do
     ra, rb = mb.read_holding_registers 5391, 2
+		# dour inputs on my Modbus hardware from Scheider
     a, b, c, d = (0..3).map {|bit| ra[bit] }
 
     puts 'a: %s' % a.to_s
@@ -21,7 +23,14 @@ begin
     puts 'c: %s' % c.to_s
     puts 'd: %s' % d.to_s
 
-    clickatell.send_message '4740220423', 'BRANN!' if a == 0
+		fire_detected = (a == 0)
+
+		if not fire_warned and fire_detected
+			puts 'Sending SMS!'
+	    clickatell.send_message '4740220423', 'BRANN!'
+		end
+
+		fire_warned = fire_detected
 
 #    [5356, 5366, 5357, 5382, 5383, 5391, 5392].each do |addr|
 #      puts '%d: %d' % [addr, mb.read_holding_registers(addr, 1).first]
