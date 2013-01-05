137672e65f9ed672f0a26d955e3d74b69e2700f0
Tallak Tveide <tallak@tveide.net>
2013-01-05 11:43:26 +0100

    Rewrote most of firealarm to use signals
    I Believe (if this is working) that this is the fist working version of
    the firealarm service

--- firealarm.rb
+++ firealarm.rb
@@ -6,65 +6,99 @@ require 'bundler/setup'
 require 'yaml'
 require 'rmodbus'
 require 'clickatell'
+require 'logger'
+
+# Simple class to monitor a signal and call
+# the block when signal is triggered
+class BoolSignal
+	def initialize
+		yield self if block_given?
+	end
+
+	def on_re(&block)
+		@on_re = block
+	end
+
+	def on_fe(&block)
+		@on_fe = block
+	end
+
+	# current value
+	def v
+		@old_v
+	end
+
+	def v=(v)
+		if !v && @old_v
+			@on_fe.call
+		end
+		if v && !@old_v
+			@on_re.call
+		end
+		@old_v = !!v
+	end
+end
 
 class FireAlarm
-	SAMPLE_TIME = 5.0
+	SAMPLE_TIME = 20.0
 	SMS_RECIPIENTS = [4740220423, 4793242788]
 	#SMS_RECIPIENTS = [4740220423]
+	attr_reader :log
 
 	def initialize
 		@test_sms_active = false
+		@log = Logger.new STDERR
+		@test_active = BoolSignal.new do |s|
+			s.on_re { send_test_sms }
+		end
+		@fire_detected = BoolSignal.new do |s|
+			s.on_re { fire_detected }
+			s.on_fe { fire_alarm_gone }
+		end
 	end
 
 	def run
-		puts 'Fire alarm started'
+		log.info 'Fire alarm started'
 		begin
 			with_modbus do |slave|
 				send_sms 'Fire alarm system started ok'
 				loop do 
-					fire_undetected_loop(slave)
-					fire_detected_loop(slave)
+					@test_active.v = is_thursday_afternoon
+					@fire_detected.v = check_fire_detected slave
+					sleep SAMPLE_TIME
 				end
+				# runs forever
 			end
 		rescue => e
-			puts e.to_s
-			puts e.backtrace
+			log.error 'Modbus connection lost'
+			log.info e
 			send_sms 'Firealarm: Modbus connection was lost'
 			retry
 		end
 	end
 
-	def is_thursday
-		Date.today.wday == 4
+	def is_thursday_afternoon
+		Date.today.wday == 4 && (Time.now.hour >= 12)
 	end
 
-	def test_sms(slave)
-		new_test_sms = is_thursday && (Time.now.hour >= 12)
-		if new_test_sms && !@test_sms_active
-			send_sms "Firealarm: weekly test, currect status is #{check_fire_detected(slave) ? 'burning' : 'ok'}"
-		end
-		@test_sms_active = new_test_sms
+	def send_test_sms
+		log.info 'Weekly test activated'
+		send_sms "Firealarm: weekly test, currect status is #{check_fire_detected(slave) ? 'burning' : 'ok'}"
 	end
 
-	def fire_undetected_loop(slave)
-		while not check_fire_detected(slave) do
-			test_sms(slave)
-			sleep SAMPLE_TIME
-		end
-	end
-
-	def fire_detected_loop(slave)
+	def fire_detected
+		log.info 'Fire detected'
 		send_sms 'Smoke or water detector activated'
-		while check_fire_detected(slave) do
-			test_sms(slave)
-			sleep SAMPLE_TIME
-		end
-		puts 'Fire alarm no longer active'
+	end
+
+	def fire_alarm_gone
+		log.info 'Fire alarm no longer active'
 	end
 
 	def with_modbus
 		ModBus::TCPClient.new '192.168.0.189' do |mb|
 			mb.with_slave 1 do |slave|
+				log.info 'Connected to modbus server'
 				yield slave
 			end
 		end
@@ -82,19 +116,22 @@ class FireAlarm
 	end
 
 	def send_sms(text)
+		log.info "Sending messages"
 		recipients = SMS_RECIPIENTS.dup
 
 		begin
 			with_clickatell do |clickatell|
+				log.info 'Connected to Clickatell'
 				while recipients.any?
-					puts 'Sending SMS to %s with the text: %s' % [recipients.first, text]
+					log.info 'Sending SMS to %s with the text: %s' % [recipients.first, text]
 					clickatell.send_message recipients.first, text
 					recipients.shift
 				end
 			end
+			log.info 'SMS sending finished'
 		rescue => e
-			puts e.to_s
-			puts e.backtrace
+			log.error 'Unable to send SMS'
+			log.info e
 			wait = 10.0
 			puts 'Could not send SMS, try again in %.0f seconds' % wait
 			sleep wait
